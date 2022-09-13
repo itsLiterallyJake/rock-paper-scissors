aef5b6e7d000d8841eedf2005e7e5e914c5f2cb1
Jake Nauman <itsliterallyjake@outlook.com>
2022-09-12 21:02:28 -0500

    Added playround function and event listeners tied to buttons

--- game.js
+++ game.js
@@ -1,7 +1,47 @@
 // Get Random Computer Choice
-const choices = ['rock', 'paper', 'scissors'];
+const choices = ['Rock', 'Paper', 'Scissors'];
 var computerChoice = "";
+var compSelect = document.getElementById('cpu-selection')
 function getCompChoice() {
     computerChoice = choices[Math.floor(Math.random()*choices.length)];
-    console.log(computerChoice);
+    compSelect.textContent = computerChoice;
 }
+
+//Plays a Round
+var roundResults = document.getElementById('round-result');
+function playRound(playerChoice, computerChoice) {
+    if (playerChoice == computerChoice) {
+        roundResults.textContent = "It's a tie!";
+    }
+    else if (playerChoice == "Rock" && computerChoice == "Paper") {
+        roundResults.textContent = "You lose, paper beats rock";
+    }
+    else if (playerChoice == 'Rock' && computerChoice == "Scissors") {
+        roundResults.textContent = "You win! Rock beats scissors!";
+    }
+    else if (playerChoice == "Paper" && computerChoice == "Rock") {
+        roundResults.textContent = "You win! Paper beats rock!";
+    }
+    else if (playerChoice == "Paper" && computerChoice == "Scissors") {
+        roundResults.textContent = "You lose, scissors beats paper";
+    }
+    else if (playerChoice == "Scissors" && computerChoice == "Rock") {
+        roundResults.textContent = "You lose, rock beats scissors";
+    }
+    else if (playerChoice == "Scissors" && computerChoice == "Paper") {
+        roundResults.textContent = "You Win! Scissors beats paper!";
+    }
+}
+
+document.getElementById('rock').addEventListener('click', function() {
+    getCompChoice();
+    playRound('Rock', computerChoice);
+})
+document.getElementById('paper').addEventListener('click' , ()=>{
+    getCompChoice();
+    playRound('Paper', computerChoice);
+})
+document.getElementById('scissors').addEventListener('click', ()=>{
+    getCompChoice();
+    playRound('Scissors', computerChoice);
+})
