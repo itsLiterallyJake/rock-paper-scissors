6dcd82a95c512cac615e85045380b3b8e4430ff5
Jake Nauman <itsliterallyjake@outlook.com>
2022-09-13 20:33:22 -0500

    Added score tracking

--- game.js
+++ game.js
@@ -7,6 +7,23 @@ function getCompChoice() {
     compSelect.textContent = computerChoice;
 }
 
+//Score tracker
+var playerScore = 0;
+var compScore = 0;
+var playerScoreText = document.getElementById('player-score');
+var compScoreText = document.getElementById('cpu-score');
+function playerWins() {
+    playerScore++
+    playerScoreText.textContent = playerScore;
+    compScoreText.textContent = compScore;
+}
+function compWins() {
+    compScore++
+    playerScoreText.textContent = playerScore;
+    compScoreText.textContent = compScore;
+}
+
+
 //Plays a Round
 var roundResults = document.getElementById('round-result');
 function playRound(playerChoice, computerChoice) {
@@ -15,21 +32,27 @@ function playRound(playerChoice, computerChoice) {
     }
     else if (playerChoice == "Rock" && computerChoice == "Paper") {
         roundResults.textContent = "You lose, paper beats rock";
+        compWins();
     }
     else if (playerChoice == 'Rock' && computerChoice == "Scissors") {
         roundResults.textContent = "You win! Rock beats scissors!";
+        playerWins();
     }
     else if (playerChoice == "Paper" && computerChoice == "Rock") {
         roundResults.textContent = "You win! Paper beats rock!";
+        playerWins();
     }
     else if (playerChoice == "Paper" && computerChoice == "Scissors") {
         roundResults.textContent = "You lose, scissors beats paper";
+        compWins();
     }
     else if (playerChoice == "Scissors" && computerChoice == "Rock") {
         roundResults.textContent = "You lose, rock beats scissors";
+        compWins();
     }
     else if (playerChoice == "Scissors" && computerChoice == "Paper") {
         roundResults.textContent = "You Win! Scissors beats paper!";
+        playerWins();
     }
 }
 
