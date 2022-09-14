22b64578275c69270613d8902ccd4eaec1e62abb
Jake Nauman <itsliterallyjake@outlook.com>
2022-09-13 22:05:40 -0500

    Added game reset function

--- game.js
+++ game.js
@@ -22,7 +22,45 @@ function compWins() {
     playerScoreText.textContent = playerScore;
     compScoreText.textContent = compScore;
 }
-
+//Game Win function
+function gameWin() {
+    if (compScore === 5) {
+        document.getElementById('final-result').textContent = "You Lose! Try again next time!";
+        gameReset();
+    }
+    else if (playerScore === 5) {
+        document.getElementById('final-result').textContent = "You Win! Its about time!";
+        gameReset();
+    }
+    else {
+        document.getElementById('final-result').textContent = "";
+    }
+}
+//Reset function
+function buttonReset(){
+    compSelect.textContent = "";
+    compScore = 0;
+    playerScore = 0;
+    playerScoreText.textContent = 0;
+    compScoreText.textContent = 0;
+    roundResults.textContent = "";
+    document.getElementById('final-result').textContent = "";
+    resetButton = document.getElementById('reset');
+    resetButton.remove();
+}
+function gameReset() {
+    let resultsCont = document.getElementById('final-cont');
+    roundResults.classList.add('r-result');
+    let resetButton = document.createElement('button');
+    resetButton.setAttribute('id', 'reset');
+    resetButton.classList.add('reset-button');
+    resetButton.textContent = "Reset";
+    resultsCont.appendChild(resetButton);
+    resetButton = document.getElementById('reset');
+    resetButton.addEventListener('click', ()=>{
+        buttonReset();
+    })
+}
 
 //Plays a Round
 var roundResults = document.getElementById('round-result');
@@ -54,6 +92,7 @@ function playRound(playerChoice, computerChoice) {
         roundResults.textContent = "You Win! Scissors beats paper!";
         playerWins();
     }
+    gameWin();
 }
 
 document.getElementById('rock').addEventListener('click', function() {
